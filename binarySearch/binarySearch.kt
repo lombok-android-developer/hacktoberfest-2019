f117cc908722a9d9382c8932cbda0a8cf3e1cef4
anwar <rilltheend@gmail.com>
2019-10-02 18:29:54 +0800

    Update and rename start.kt to binarySearch.kt

--- binarySearch/binarySearch.kt
+++ binarySearch/binarySearch.kt
@@ -6,12 +6,11 @@ fun main(){
     val list = mutableListOf('a', 'b', 'c', 'd', 'e','f','g','h','i',
         'j','k','l','m','n','o','p','q','r','s',
         't','u','v','w','x','y','z')
-    val a = list.binarySearch('c')
-    val b = list.binarySearch('g')
-    val c = list.binarySearch('f')
-    println("Event = $name")
-    println("Lokasi = $location")
-    println("Tanggal = $a$b,$c,${list.binarySearch('u')}${list.binarySearch('t')}") // 3
-
-
+    ..
+    ..
+    ..
+    ..
+    ..
+    println()
+ 
 }
